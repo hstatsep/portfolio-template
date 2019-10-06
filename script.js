e75b6e244931e632b7874c99601dc3400200b86d
Mr. Mueller <bmueller@hstat.org>
2019-10-06 18:41:59 +0000

    update nav and footer

---
+++ script.js
@@ -0,0 +1,4 @@
+let username = "bmuellerhstat";
+
+document.querySelector("#github").href = "http://github.com/"+username;
+document.querySelector("#fork").href = "http://github.com/"+username+"/"+username+".github.io";
