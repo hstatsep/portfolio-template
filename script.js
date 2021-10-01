48d5e70310d28feb2a172ee615124850aa2f1de0
Mr. Mueller <bmueller@hstat.org>
2021-10-01 11:44:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
+let userName = "YourUsername"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
